ed6b58502f47352d1690faceadfa7794e691375a
Zachary Kieda <zkieda@tests-MBP.wv.cc.cmu.edu>
2015-10-13 13:59:19 -0400

    cleaning

--- ModLang/src/edu/cmu/cs464/p3/modulelang/ast/ParsedModule.scala
+++
@@ -1,8 +0,0 @@
-package edu.cmu.cs464.p3.modulelang.ast
-
-import java.util.{List => JList}
-
-/**
- * @author zkieda
- */
-case class ParsedModule(baseClass : JavaRef, children : JList[ParsedModule]) extends Node{}
--- ModLang/src/edu/cmu/cs464/p3/modulelang/ast/Program.scala
+++
@@ -1,8 +0,0 @@
-package edu.cmu.cs464.p3.modulelang.ast
-
-import java.util.{List => JList};
-
-/**
- * @author zkieda
- */
-case class Program(imports : JList[ModuleImport], baseModule : ParsedModule) extends Node{}
